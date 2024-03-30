4207d4f55be0738d0baa496e833d8afec197513e
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-03-30 16:17:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 //your JS code here. If required.
-<script>
-    function togglePanel(panelNumber) {
-        const panel = document.getElementById(`panel-${panelNumber}`);
-        const panels = document.querySelectorAll('.panel');
-
-        panels.forEach((p) => {
-            p.classList.remove('active');
-        });
-
-        panel.classList.add('active');
-    }
-</script>
-
-</body>
-</html>
